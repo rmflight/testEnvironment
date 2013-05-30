68119d7aa78c39c8cd5046a7a8642b00a787f7a2
rmflight <r.flight79@gmail.com>
2013-05-29 22:00:09 -0400

    updated documentation with description of parameters

--- R/testEnvironment.R
+++ R/testEnvironment.R
@@ -6,6 +6,7 @@ showAll <- function(){
 
 #' display a specific set of variables
 #' @export
+#' @param which_vars the variable names you want to display as strings
 showVar <- function(which_vars){
   invisible(sapply(which_vars, function(x){
     out_var <- get(x, envir=testEnvironment_data)
@@ -15,6 +16,8 @@ showVar <- function(which_vars){
 
 #' assign a variable to the environment
 #' @export
+#' @param var_name the name to give to the variable as a string
+#' @param var_value the value to assign to \code{var_name}
 assignVar <- function(var_name, var_value){
   assign(var_name, var_value, envir=testEnvironment_data)
 }
